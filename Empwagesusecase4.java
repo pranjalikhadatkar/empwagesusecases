c0662c9d880f766cc0826b92d183e9cdf82b68d4
pranjalikhadatkar <94820061+pranjalikhadatkar@users.noreply.github.com>
2021-12-24 00:48:10 +0530

    Use Case 4

---
+++ Empwagesusecase4.java
@@ -0,0 +1,34 @@
+package fundamentals;
+//usecase4
+
+public class Empwagesusecase4 {
+	
+	static final int IS_PART_TIME = 1;
+	static final int IS_FULL_TIME = 2;
+	
+	
+	public static void main(String[] args) {
+		//variables
+		 
+		int EMP_WAGES_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWages = 0;
+		
+		int empcheck = (int)Math.round(Math.random());
+		switch (empcheck)
+		{
+		 case IS_PART_TIME:
+			 		empHrs = 4;
+			 		break;
+		 case IS_FULL_TIME:
+			 		empHrs = 8;
+			 		break;
+		 default:
+			 	empHrs = 0;
+			 	break;
+		}
+		empWages = empHrs * EMP_WAGES_PER_HOUR;
+		System.out.println("emp wages:" + empWages);
+		}
+		
+	}
