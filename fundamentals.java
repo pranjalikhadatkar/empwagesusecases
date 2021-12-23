65fc058798ce313900e56a84b244f258bd7c3abd
pranjalikhadatkar <94820061+pranjalikhadatkar@users.noreply.github.com>
2021-12-23 14:05:04 +0530

    added uc1

---
+++ fundamentals.java
@@ -0,0 +1,17 @@
+package fundamentals;
+public class fundamentals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int IS_FULL_TIME = 1;
+			double empCheck = Math.floor(Math.random() * 10 ) % 2;
+	
+	         if (empCheck == IS_FULL_TIME)
+				System.out.println("employee is present");
+			else
+				System.out.println("employee is absent");
+	}
+
+}
+
